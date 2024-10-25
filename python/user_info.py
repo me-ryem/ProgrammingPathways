31b9dcc04dcd8b724dcfbbef7d5c9612a813b905
Maryam Arif <156967591+me-ryem@users.noreply.github.com>
2024-10-25 21:30:45 +0500

    Update user_info.py
    Added an input-check, in case of non-numeric input for age, it displays an error message and exits.

--- python/user_info.py
+++ python/user_info.py
@@ -1,7 +1,15 @@
-# A program that asks the user for their name and age. The program should then display a greeting message that includes their name and age. Additionally, it should check whether the user is an adult (18 years or older) or a minor (under 18) and print an appropriate message based on their age.
+import sys
 
 name = input("Enter your name: ")
-age = int(input("Enter your age: "))
+age = input("Enter your age: ")
+
+# input validation
+try:
+    age = int(age)
+except ValueError:
+    print(f"{age} is invalid.")
+    sys.exit(1)
+
 
 print(f"Merhaba, {name}! You're {age} years of age.")
 
