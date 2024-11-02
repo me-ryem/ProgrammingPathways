713f0968e17a66eb65a8a1a541b6d80c39889f61
Maryam Arif <156967591+me-ryem@users.noreply.github.com>
2024-11-02 11:52:21 +0500

    Create file.cpp
    Reads text from one file, and writes it to another.

---
+++ cpp/file/file.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    // for writing
+    ofstream myOutput; // creating an object for the file to be written to
+    myOutput.open("C:/Users/marya/3D Objects/Programming/Files/sample-copied.txt"); 
+
+    // for reading
+    ifstream myInput; // creating an object for the file to be read
+    myInput.open("C:/Users/marya/3D Objects/Programming/Files/sample-to-be-copied.txt");
+
+    if (myInput && myOutput)
+    {
+        string line; 
+        while(getline(myInput, line)) // reading line by line
+            myOutput << line << endl; // writing
+    }
+    else // in case of error
+    {
+        cerr << "File couldn't be opened.";
+        return 1; 
+    }
+
+    // closing the files
+    myOutput.close();
+    myInput.close();
+
+    return 0;
+}
